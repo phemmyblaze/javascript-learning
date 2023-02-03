3a7855f76158da234d9bfa205f9f2e030f62407e
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-02-03 17:37:38 +0100

    Learning day 2

--- script/script.js
+++ script/script.js
@@ -86,3 +86,135 @@ if (markBMI2 > johnBMI2) {
 } else {
     console.log(`mark's bmi is ${markBMI2} is less than ${johnBMI2}`)
 }
+
+//TYPE CONVERSION / TYPE COERCION
+
+const courrentYear = "2023";
+console.log(courrentYear)
+
+console.log(courrentYear + 18)
+console.log(Number(courrentYear) + 18);
+
+console.log(Number('bylaze'))
+console.log(String('23'))
+
+
+//TYPE COERCION
+console.log(24 < 18)
+
+
+let height = 1.76;
+
+if (height) {
+    console.log(`Height is defined! You are ${height} m tall
+    keep growing tall
+    `)
+
+} else if (height == 0) {
+    console.log (`height is ${height} you have to grow taller`)
+} else  {
+    console.log(`Height is not defined`)
+}
+
+
+//TRUTHY / FALSY
+
+//AND OPERATOR &&
+
+let age = 16;
+
+if (age >= 20 && age <= 30) {
+    console.log(`You are ${age} years old`)
+} else  {
+    console.log(`20 is greater than your age but you are less than 30 `)
+}
+
+// OR OPERATOR ||
+
+let age2 = 60;
+
+if (age2 == 20 || age ==30) {
+    console.log(`Awesome you are ${age2} years old`)
+} else {
+    console.log(`Age is greater and equal to 30`)
+}
+
+
+///// CODING CHALLENGE 3
+
+/* 
+
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+let scoreDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110) / 3;
+let minScore = 100;
+console.log(scoreDolphins, scoreKoalas);
+
+//1. TESTING WHICH TEAM WINS THE TROPHY
+
+if (scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins wins the trophy! 🏆`)
+} else if (scoreDolphins === scoreKoalas) {
+    console.log(`This is a draw no team wins the trophy 📍`)
+} else {
+    console.log(`Koalas wins the trophy! 🏆`)
+}
+
+console.log(`Score 2`)
+
+//2. TEST FOR QUESTION 2
+let scoreDolphins2 = (97 + 112 + 101) / 3;
+let scoreKoalas2 = (109 + 95 + 123) / 3;
+let minScore2 = 100;
+console.log(scoreDolphins2, scoreKoalas2);
+
+if (scoreDolphins2 <= minScore2) {
+    console.log(`Dolphins wins the trophy! with a score of ${scoreDolphins2}`)
+} else if (scoreKoalas2 >= minScore2) {
+    console.log(`Koalas wins the trophy! with a score of ${scoreKoalas2}`)
+} else {
+    console.log(`No on wins try harder next season ���`);
+}
+
+
+console.log(`Score 3`)
+
+//3. TEST FOR QUESTION 3  Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+let scoreDolphins3 = (97 + 112 + 101) / 3;
+let scoreKoalas3 = (109 + 95 + 126) / 3;
+let minScore3 = 100;
+console.log(scoreDolphins3, scoreKoalas3);
+
+if (scoreDolphins3 > minScore3 &&  scoreDolphins3 > scoreKoalas3) {
+    console.log(`Dolphins wins the trophy! with a score of ${scoreDolphins3}`)
+} else if (scoreKoalas3 > minScore3 && scoreKoalas3 > scoreDolphins3) {
+    console.log(`Koalas wins the trophy! with a score of ${scoreKoalas3}`)
+} else {
+    console.log(`No on wins try harder next season ���`);
+}
+
+
+
+
+
+
+
