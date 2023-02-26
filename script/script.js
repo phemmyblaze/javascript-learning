0dac02a60211f1f61b6aa4453257c65f7ecedc33
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-02-26 22:15:21 +0100

    learning continues

--- script/script.js
+++ script/script.js
@@ -933,6 +933,114 @@ printGoals(...game.scored)
 team1 < team2 && console.log(`team1 is more likely to win`) 
 
 
+///question 1
+// let goalsScores = Object.entries(game.scored)
+
+for (const [i, players] of game.scored.entries()){
+    
+    console.log(`Goal ${i + 1} score: ${players}`)
+}
+
+
+///question 2
+let aver =0;
+const odds = Object.values(game.odds)
+for (const odd of odds) aver += odd; 
+aver /= odds.length;
+console.log(aver)
+
+///question 3
+
+for (const [team, odd]of Object.entries(game.odds)) {
+    const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`
+
+    console.log(`odd of ${teamStr} ${odd}`)
+}
+
+
+///QUESTION 4
+
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+
+  console.log(player)
+}
+
+
+/**** 
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+
+ const gameEvents = new Map([
+ [17, '⚽ GOAL'],
+ [36, '� Substitution'],
+ [47, '⚽ GOAL'],
+ [61, '� Substitution'],
+ [64, '� Yellow card'],
+ [69, '� Red card'],
+ [70, '� Substitution'],
+ [72, '� Substitution'],
+ [76, '⚽ GOAL'],
+ [80, '⚽ GOAL'],
+ [92, '� Yellow card'],
+ ]);
+******/
+
+///question 1 
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '� Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '� Substitution'],
+    [64, '� Yellow card'],
+    [69, '� Red card'],
+    [70, '� Substitution'],
+    [72, '� Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '� Yellow card'],
+]);
+
+
+
+const events = [...new Set(gameEvents.values())]
+console.log(events);
+
+gameEvents.delete(64)
+
+console.log(`An event happens on average, ${90 / gameEvents.size } minutes`)
+
+
+
+
+for (const [min, event] of gameEvents) {
+    const half = min <= 45 ? 'FIRST HALF' : 'SECOND HALF';
+    console.log(`${half} ${min}: ${event}`)
+}
+
+console.log(gameEvents);
+
+
+
+// const staff = ['waiter', 'chef', 'waiter', 'manager', 'chef', 'manager']
+
+// const staffset = [...new Set(staff)];
+// console.log(staffset)
+
 
 
 
