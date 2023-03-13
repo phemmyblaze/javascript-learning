c156507bfab9f7e3f2bfb55dbd0e24204ff505b3
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-03-13 17:28:30 +0100

    More Learning

--- script/script.js
+++ script/script.js
@@ -1130,6 +1130,15 @@ const matchingValues = retrievedArray.filter(str => str.match(regex));
 
 console.log(matchingValues); 
 
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red'; 
+    const body = document.querySelector('body');
+    body.addEventListener('click', function () {
+        header.style.color = 'blue';
+    })
+}());
+
 
 
 
